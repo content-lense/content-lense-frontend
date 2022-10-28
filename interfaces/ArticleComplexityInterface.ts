f89028ce75d17e7bc8be952a7c3d2228e44c3a45
Jonas Trittler <jonas@cloud-creators.de>
2022-10-28 15:32:01 +0200

    added text complexity batches

--- interfaces/ArticleComplexityInterface.ts
+++ interfaces/ArticleComplexityInterface.ts
@@ -4,4 +4,6 @@ export interface ArticleComplexityInterface{
     article: ArticleInterface;
     wienerSachtextIndex: number;
     readingTimeInMinutes: number;
+    totalWords: number;
+    part: string;
 }
